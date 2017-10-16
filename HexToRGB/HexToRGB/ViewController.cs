475e63ca10dbc3391d309d8a7d870f7a550ba530
XxJadenxX <32143231+XxJadenxX@users.noreply.github.com>
2017-10-16 14:01:40 -0600

    the converter i think finally finished

--- HexToRGB/HexToRGB/ViewController.cs
+++ HexToRGB/HexToRGB/ViewController.cs
@@ -30,6 +30,15 @@ namespace HexToRGB
             string redHexValue = hexValue.Substring(0, 2);
             string greenHexValue = hexValue.Substring(2, 2);
             string blueHexValue = hexValue.Substring(4, 2);
+
+            int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+            int greenValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+            int blueValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+
+            redValueLabel.Text = redValue.ToString ();
+            greenValueLabel.Text = greenValue.ToString ();
+            blueValueLabel.Text = blueValue.ToString ();
+
         }
         public override void DidReceiveMemoryWarning()
         {
